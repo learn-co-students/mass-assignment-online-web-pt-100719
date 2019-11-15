ae41b1ab6290404d49bb92eeb87119a3800264f5
PBillingsby <philbillingsby@gmail.com>
2019-11-14 19:29:37 -0500

    Testing with different hashes

---
+++ .history/lib/person_20191114192625.rb
@@ -0,0 +1,12 @@
+class Person
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
+  :weight, :handed, :complexion, :t_shirt_size, 
+  :wrist_size, :glove_size, :pant_length, :pant_width
+  def initialize(attributes)
+    attributes.each do |k, v| 
+      self.send(("#{k}="), v)#your code here
+    end
+  end
+end
+
+wayne_attributes{name: "wayne", birthdya: 1986, :hair_color "Brown", :eye_color "Blue", :height 1.73, :weight 73, :handed "Right", :complexion "Pale", :t_shirt_size "L", :wrist_size "Small", :glove_size "small", :pant_length 32, :pant_width 10}
